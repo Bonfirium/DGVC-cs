037b0f2e7469cb92eecc3f2ad9e1ff0a8b20ad3d
Kriket <Kriket267@gmail.com>
2017-09-09 16:25:28 +0300

    AsyncSqr

--- DGVC_UI/FormWelcome.cs
+++ DGVC_UI/FormWelcome.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Forms;
 using TestProject;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DGVC_UI
 {
@@ -8,7 +10,25 @@ namespace DGVC_UI
         public FormWelcome()
         {
             InitializeComponent();
-            label_UsersCount.Text = "Зарегестрированно пользователей: " + GolosManager.GetUsersCount();
+            Label[] labels = new Label[] { label_UsersCount, label1, label2, label3, label4, label5, label6, label7, label8, label9 };
+            foreach (var item in labels)
+            {
+                DisplayResultAsync(item);
+            }
+            
+        }
+
+        static async void DisplayResultAsync(Label label)
+        {
+            label.Text = "Зарегистрированно пользователей: " + await AsyncUsersCount();
+        }
+
+        static Task<ulong> AsyncUsersCount()
+        {
+            return Task.Run(() =>
+            {
+                return GolosManager.GetUsersCount();
+            });
         }
     }
 }
